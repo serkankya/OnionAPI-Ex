701b4a2bfb596e54c2f3106b9cb34db687c0cecf
Serkan Kaya <serkankaya0721@gmail.com>
2025-10-19 17:51:12 +0300

    configure IMapper and ICustomMapper

--- Project.Application/Features/Products/Queries/GetAllProductsQueryHandler.cs
+++ Project.Application/Features/Products/Queries/GetAllProductsQueryHandler.cs
@@ -1,4 +1,7 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Project.Application.DTOs.BrandDTOs;
+using Project.Application.Interfaces.CustomAutoMapper;
 using Project.Application.Interfaces.UnitOfWorks;
 using Project.Domain.Entities;
 
@@ -7,30 +10,26 @@ namespace Project.Application.Features.Products.Queries
 	public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, IList<GetAllProductsQueryResponse>>
 	{
 		private readonly IUnitOfWork _unitOfWork;
+		private readonly ICustomMapper _mapper;
 
-		public GetAllProductsQueryHandler(IUnitOfWork unitOfWork)
+		public GetAllProductsQueryHandler(IUnitOfWork unitOfWork, ICustomMapper mapper)
 		{
 			_unitOfWork = unitOfWork;
+			_mapper = mapper;
 		}
 
 		public async Task<IList<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
 		{
-			var products = await _unitOfWork.GetReadRepository<Product>().GetAllAsync();
+			var products = await _unitOfWork.GetReadRepository<Product>().GetAllAsync(include: x => x.Include(b => b.Brand));
 
-			List<GetAllProductsQueryResponse> response = new();
+			var brand = _mapper.Map<BrandDto, Brand>(new Brand());
 
-			foreach (var product in products)
-			{
-				response.Add(new GetAllProductsQueryResponse
-				{
-					Title = product.Title,
-					Description = product.Description,
-					Discount = product.Discount,
-					Price = product.Price - (product.Price * product.Discount / 100)
-				});
-			}
+			var map = _mapper.Map<GetAllProductsQueryResponse, Product>(products);
 
-			return response;
+			foreach (var item in map)
+				item.Price -= (item.Price * item.Discount / 100);
+
+			return map;
 		}
 	}
 }
---
+++ Project.Mapper/Registration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.Application.Interfaces.CustomAutoMapper;
+using Project.Mapper.CustomAutoMapper;
+
+namespace Project.Mapper
+{
+	public static class Registration
+	{
+		public static void AddCustomMapper(this IServiceCollection services)
+		{
+			services.AddSingleton<ICustomMapper, CustomMapper>();
+		} 
+	}
+}
