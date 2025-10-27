b1b7f796e61a743b86cd41f16e4b4a0b6baab215
Serkan Kaya <serkankaya0721@gmail.com>
2025-10-27 19:37:26 +0300

    add bearer configurations

--- Project.Infrastructure/Registration.cs
+++ Project.Infrastructure/Registration.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Project.Application.Interfaces.Tokens;
 using Project.Infrastructure.Tokens;
+using System.Text;
 
 namespace Project.Infrastructure
 {
@@ -9,6 +13,28 @@ namespace Project.Infrastructure
 		public static void AddInfrastructure(this IServiceCollection services, IConfiguration config)
 		{
 			services.Configure<TokenSettings>(config.GetSection("JWT"));
+
+			services.AddTransient<ITokenService, TokenService>();
+
+			services.AddAuthentication(opt =>
+			{
+				opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+			{
+				opt.SaveToken = true;
+				opt.TokenValidationParameters = new TokenValidationParameters()
+				{
+					ValidateIssuer = false,
+					ValidateAudience = false,
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWT:Secret"])),
+					ValidateLifetime = false,
+					ValidIssuer = config["JWT:Issuer"],
+					ValidAudience = config["JWT:Audience"],
+					ClockSkew = TimeSpan.Zero
+				};
+			});
 		}
 	}
 }
--- Project.WebAPI/Program.cs
+++ Project.WebAPI/Program.cs
@@ -4,6 +4,7 @@ using Project.Persistence;
 using Project.Mapper;
 using Project.Application.Exceptions;
 using Project.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 namespace Project.WebAPI
 {
@@ -16,7 +17,39 @@ namespace Project.WebAPI
 			// Add services to the container.
 
 			builder.Services.AddControllers();
-			builder.Services.AddSwaggerGen();
+			builder.Services.AddSwaggerGen(s =>
+			{
+				s.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Title = "API Proejct",
+					Version = "v1",
+					Description = "Onion Architecture & Clean Architecture - JWT Auth Example"
+				});
+				s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+				{
+					Name = "Authorization",
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "bearer",
+					BearerFormat = "JWT",
+					In = ParameterLocation.Header,
+					Description = "Enter 'Bearer' followed by your JWT token. Example: **Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...**"
+				});
+				s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+				{
+					{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			Array.Empty<string>()
+				}
+				});
+			});
+
 			// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 			builder.Services.AddOpenApi();
 
@@ -24,8 +57,8 @@ namespace Project.WebAPI
 
 			builder.Configuration
 				.SetBasePath(env.ContentRootPath)
-				.AddJsonFile("appsettings.json", optional: false)
-				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+						.AddJsonFile("appsettings.json", optional: false)
+						.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
 			builder.Services.AddPersistence(builder.Configuration);
 			builder.Services.AddInfrastructure(builder.Configuration);
